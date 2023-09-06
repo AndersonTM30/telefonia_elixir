347846a826130e9a151e3f7481705cfcb2d4dae9
Anderson Ferreira <anderson@pop-os.localdomain>
2023-09-06 16:39:23 -0300

    feat: criando a função write e read para salvar e ler os dados no txt criado para salvar a lista de assinantes

--- telefonia/lib/assinante.ex
+++ telefonia/lib/assinante.ex
@@ -3,9 +3,20 @@ defmodule Assinante do
   defstruct nome: nil, numero: nil, cpf: nil
 
   def cadastrar(nome, numero, cpf) do
-    assinante = %__MODULE__{nome: nome, numero: numero, cpf: cpf}
+    read() ++ [%__MODULE__{nome: nome, numero: numero, cpf: cpf}]
     |> :erlang.term_to_binary()
+    |> write()
 
-    File.write('assinantes.txt', assinante)
+  end
+
+  # função privada
+  defp write(lista_assinantes) do
+    File.write!("assinantes.txt", lista_assinantes)
+  end
+
+  def read() do
+    {:ok, assinantes} = File.read("assinantes.txt")
+    assinantes
+    |> :erlang.binary_to_term()
   end
 end
