fd2f9ac790c2ab3d75c082d5e09c57782e48a511
Anderson Ferreira <anderson@pop-os.localdomain>
2023-09-06 16:15:03 -0300

    feat: separando as responsabilidades e salvando os assinantes em txt

--- telefonia/lib/assinante.ex
+++ telefonia/lib/assinante.ex
@@ -1,4 +1,11 @@
 # criando um struct
 defmodule Assinante do
   defstruct nome: nil, numero: nil, cpf: nil
+
+  def cadastrar(nome, numero, cpf) do
+    assinante = %__MODULE__{nome: nome, numero: numero, cpf: cpf}
+    |> :erlang.term_to_binary()
+
+    File.write('assinantes.txt', assinante)
+  end
 end
